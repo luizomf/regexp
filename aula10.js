618c6bea2455d234ee3069007f4c88779b078d7c
Luiz Otávio <luizomf@gmail.com>
2020-01-19 18:21:37 -0300

    Comentários

--- aula10.js
+++ aula10.js
@@ -16,7 +16,10 @@ const validaTelefone = /^(\(\d{2}\)\s*)?(9\s*)?(\d{4})-(\d{4})$/g
 // Validar senhas fortes
 const validaSenhasFortes = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%\]\)]).{8,}$/g
 
-// Validar e-mails
+// Validar e-mails (use a última)
 // [^\s]+@[^\s]+\.[^\s]+(\w+)* (Permissiva)
 // [^\s\.]+\.?[^\s\.]+@[^\s]+\.[^\s]+\w+
+
+// Atenção: em meus testes a regexp que obteve melhor resultado 
+// foi (acertando mais válidos e inválidos) a da linha a seguir:
 // /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
