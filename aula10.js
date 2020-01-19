42af8c3075e0f6b78e3d5101e173ca22322a0926
Luiz Otávio <luizomf@gmail.com>
2020-01-19 18:27:56 -0300

    Comentários

--- aula10.js
+++ aula10.js
@@ -16,10 +16,17 @@ const validaTelefone = /^(\(\d{2}\)\s*)?(9\s*)?(\d{4})-(\d{4})$/g
 // Validar senhas fortes
 const validaSenhasFortes = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%\]\)]).{8,}$/g
 
-// Validar e-mails (use a última)
-// [^\s]+@[^\s]+\.[^\s]+(\w+)* (Permissiva)
+// ##################################
+// # Validar e-mails (use a última) #
+// ##################################
+
+// [^\s]+@[^\s]+\.[^\s]+(\w+)* (Hiper Permissiva)
+
+// Essa expressão é bem permissiva, menos que a anterior, 
+// Mas ainda detecta bastante e-mails inválidos
 // [^\s\.]+\.?[^\s\.]+@[^\s]+\.[^\s]+\w+
 
-// Atenção: em meus testes a regexp que obteve melhor resultado 
-// foi (acertando mais válidos e inválidos) a da linha a seguir:
+// Em meus testes a regexp que obteve melhor resultado 
+// (acertando mais válidos e inválidos) 
+// Foi a da linha a seguir:
 // /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
